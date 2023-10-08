c4e506bcce6be3fdca99244e457e1faeb46b995f
darcy-bcit <D'Arcy_D_Smith@bcit.ca>
2023-10-08 00:20:05 -0700

    initial commit

---
+++ display.c
@@ -0,0 +1,7 @@
+#include "display.h"
+#include <stdio.h>
+
+void display(const char *msg)
+{
+    printf("%s\n", msg);
+}
---
+++ main.c
@@ -0,0 +1,9 @@
+#include "display.h"
+#include <stdlib.h>
+
+int main(void)
+{
+    display("Hello, World");
+
+    return EXIT_SUCCESS;
+}
